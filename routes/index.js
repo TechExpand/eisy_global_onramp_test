4362192893c9d8c92c4ff777b3ed5dc7597f0809
TechExpand <dailydevo9@gmail.com>
2024-05-15 18:10:01 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,66 @@
 const { Router } = require('express');
-
+const puppeteer = require('puppeteer');
 const routes = Router();
 
 // index page
-routes.get('/', function (req, res) {
-    res.render('pages/index');
+routes.get('/', async function (req, res) {
+    const browser = await puppeteer.launch({
+        headless: true,
+        defaultViewport: null,
+        // dumpio: true
+    });
+    const page = await browser.newPage();
+    await page.goto('https://pay.radom.com/invoice/6e45f340-6f0a-400d-b59b-668917f78c2c', { waitUntil: 'networkidle2' });
+    await page.waitForSelector('#app');
+    await page.waitForSelector('button.sc-gEvEer.gtPejK');
+    await page.click('button.sc-gEvEer.gtPejK');
+    await page.waitForSelector('div.sc-cwHptR.gFHeKo');
+    const spanContents = await page.evaluate(() => {
+        const spans = document.querySelectorAll('span');
+        return Array.from(spans).map(span => span.innerText);
+    });
+
+
+    let address = "";
+
+    spanContents.forEach(current => {
+        if (current.length > address.length) {
+            address = current;
+        }
+    });
+
+    console.log(address)
+
+    await browser.close();
+    res.render('pages/index', { address, invoiceId: "invoiceId" });
 });
+
+routes.get('/sell', async function (req, res) {
+    res.render('pages/sell', { invoiceId: "invoiceId" });
+});
+
+// routes.get('/api/fiat', async function (req, res) {
+
+// });
+
+
+
 
 
 
 // webhook request
 routes.post('/api/webhook', function (req, res) {
-    const data = req.body;  
+    const data = req.body;
+    const value = JSON.stringify(data)
     console.log(`Received data: ${JSON.stringify(data)}`);
+    if (value.data.status == "completed") {
+        console.log(value.externalCustomerId)
+        console.log("update invoice on successful")
+        console.log("success...")
+    } else {
+        console.log("update invoice on failed")
+        console.log("failed...")
+    }
     res.status(200).json({ status: 'success' });
 });
 
