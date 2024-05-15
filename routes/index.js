a7eec9986d03df03a9ab4049c77db9d51b43cd4d
TechExpand <dailydevo9@gmail.com>
2024-05-15 12:59:04 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,16 @@ routes.get('/', function (req, res) {
 
 // webhook request
 routes.post('/api/webhook', function (req, res) {
-    console.log(req.body)
-    res.send('Ok');
+    const data = req.body;  
+    console.log(`Received data: ${JSON.stringify(data)}`);
+    res.status(200).json({ status: 'success' });
 });
+
+
+// // webhook request
+// routes.get('/api', function (req, res) {
+//     res.send('Ok');
+// });
 
 
 module.exports = routes;
